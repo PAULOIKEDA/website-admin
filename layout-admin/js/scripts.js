9b419ab2ee3518190d8e89c88f536b5f3066a4ac
Danrlei Dal Fre <danrleidalfre@gmail.com>
2018-12-10 22:47:15 -0200

    barra lateral responsiva e adicionado icones do painel admin

--- layout-admin/js/scripts.js
+++ layout-admin/js/scripts.js
@@ -11,3 +11,15 @@ ScrollReveal().reveal(
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(document).ready(function() {
+    $('.sidebar-toogle').on('click', function() {
+        $('.sidebar').toggleClass('toggled');
+    });
+    var active = $('.sidebar .active');
+    if (active.length && active.parent('.collapse').length) {
+        var parent = active.parent('.collapse');
+        parent.prev('a').attr('aria-expanded', true);
+        parent.addClass('show');
+    }
+});
