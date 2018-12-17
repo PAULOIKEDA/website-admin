c98fb07614e68f0e0d6e4aaae34cd8637b838ba0
Danrlei Dal Fre <danrleidalfre@gmail.com>
2018-12-17 00:48:45 -0200

    removendo acentos e caracteres especiais da URL e convertendo para classe

--- back/core/ConfigController.php
+++ back/core/ConfigController.php
@@ -5,10 +5,54 @@ namespace Core;
 class ConfigController {
 
     private $Url;
+    private $UrlConjunto;
+    private $UrlController;
+    private $UrlParametro;
+    private static $Format;
 
     public function __construct() {
-        $this->Url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
-        echo "URL : {$this->Url} <br>";
+        if (!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))) {
+            $this->Url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
+            $this->limparUrl();
+            $this->UrlConjunto = explode("/", $this->Url);
+
+            if (isset($this->UrlConjunto[0])) {
+                $this->UrlController = $this->slugController($this->UrlConjunto[0]);
+            } else {
+                $this->UrlController = "home";
+            }
+
+            if (isset($this->UrlConjunto[1])) {
+                $this->UrlParametro = $this->UrlConjunto[1];
+            } else {
+                $this->UrlParametro = null;
+            }
+
+            echo "URL: {$this->Url} <br>";
+            echo "Controller: {$this->UrlController}";
+        } else {
+            $this->UrlController = "Home";
+            $this->UrlParametro = null;
+        }        
+    }
+
+    private function limparUrl() {
+        $this->Url = strip_tags($this->Url);
+        $this->Url = trim($this->Url);
+        $this->Url = rtrim($this->Url, "/");
+        self::$Format = array();
+        self::$Format['a'] = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿRr"!@#$%&*()_-+={[}]?;:.,\\\'<>°ºª ';
+        self::$Format['b'] = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr--------------------------------';
+        $this->Url = strtr(utf8_decode($this->Url), utf8_decode(self::$Format['a']), self::$Format['b']);
+    }
+
+    private function slugController($SlugController) {
+        $UrlController = strtolower($SlugController);
+        $UrlController = explode("-", $UrlController);
+        $UrlController = implode(" ", $UrlController);
+        $UrlController = ucwords($UrlController);
+        $UrlController = str_replace(" ", "", $UrlController);
+        return $UrlController;
     }
 
 }
