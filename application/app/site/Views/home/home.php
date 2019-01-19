0d0b255162b11f03e5d35c2f1c9b175b786d4039
Danrlei Dal Fre <danrleidalfre@gmail.com>
2019-01-18 22:37:58 -0200

    finalizada sessão de soluções e corrigido erro

--- application/app/site/Views/home/home.php
+++ application/app/site/Views/home/home.php
@@ -45,4 +45,34 @@
     </div>
 </section>
 
-<?php echo($this->Dados['solucoes']); ?>
+<?php extract($this->Dados['solucoes'][0]) ?>
+<section>
+    <div class="jumbotron rounded-0 mb-0">
+        <div class="container">
+            <h2 class="text-center mb-5"><?php echo $titulo; ?></h2>
+            <div class="card-deck">
+                <div class="card text-center animation">
+                    <i class="far fa-<?php echo $icone_1; ?> fa-10x mt-4"></i>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $nome_1; ?></h5>
+                        <p class="card-text text-muted"><?php echo $descricao_1; ?></p>
+                    </div>
+                </div>
+                <div class="card text-center animation">
+                    <i class="far fa-<?php echo $icone_2; ?> fa-10x mt-4"></i>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $nome_2; ?></h5>
+                        <p class="card-text text-muted"><?php echo $descricao_2; ?></p>
+                    </div>
+                </div>
+                <div class="card text-center animation">
+                    <i class="far fa-<?php echo $icone_3; ?> fa-10x mt-4"></i>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $nome_3; ?></h5>
+                        <p class="card-text text-muted"><?php echo $descricao_3; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
