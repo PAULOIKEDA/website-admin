d11645e3025af98268a8e30a462effd565e7710e
Danrlei Dal Fre <danrleidalfre@gmail.com>
2019-01-20 20:10:10 -0200

    inserido footer no rodapé

--- application/app/site/Views/include/rodape.php
+++ application/app/site/Views/include/rodape.php
@@ -1,3 +1,9 @@
+        <footer>
+            <div class="footer text-center py-4 bg-dark text-white">
+                &copy; dalfre soluções
+            </div>
+        </footer>
+        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="<?php echo URL; ?>app/assets/js/bootstrap.min.js"></script>
