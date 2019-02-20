88426e2b0124803e101918668567255288f2509a
Danrlei Dal Fré <danrleidalfre@gmail.com>
2019-02-19 22:55:01 -0300

    migração de site remoto para dalfre.com

--- application/core/Config.php
+++ application/core/Config.php
@@ -3,13 +3,13 @@
 session_start();
 ob_start();
 
-define('URL', 'http://localhost/mvc-na-unha/application/');
-define('URLADM', 'http://localhost/mvc-na-unha/application/admin/');
+define('URL', 'https://dalfre.com/solucoes/site/application/');
+define('URLADM', 'https://dalfre.com/solucoes/site/application/admin/');
 
 define('CONTROLLER', 'Home');
 define('METODO', 'index');
 
 define('HOST', 'localhost');
-define("USER", 'user');
-define('PASS', 'root');
-define('DBNAME', 'dalfre');
+define("USER", 'dalfre32_dalfre');
+define('PASS', 'root1234');
+define('DBNAME', 'dalfre32_site_admin');
